0062f337d6fcb2e4dc0629a4d2d81ea465f8f975
MemoOR <dethkarz2009@hotmail.com>
2023-11-10 15:58:39 -0600

    fix dark light mode

--- web/app/static/js/dark_mode.js
+++ web/app/static/js/dark_mode.js
@@ -2,30 +2,28 @@
 function setDarkMode() {
     $("#DarkLightToggle").text("Light Mode");
     $(".page").addClass("bg-dark light-content")
+    $(".home-section, .small-section, .main-footer-div").removeClass("bg-dark-alfa-50")
+    $(".home-section, .small-section, .main-footer-div").addClass("bg-dark-alfa-70")
     $(".main-nav").removeClass("light-after-scroll")
     $(".divider").addClass("white")
     $(".page-section").addClass("bg-dark light-content")
-    $(".home-section").toggleClass("bg-dark-alfa-50 bg-dark-alfa-70")
     $("#work-grid").removeClass("hover-white")
-    $(".small-section").toggleClass("bg-dark-alfa-50 bg-dark-alfa-70")
     $(".call-action-btn").addClass("btn-w")
     $(".submit_btn").addClass("btn-w")
-    $(".main-footer-div").toggleClass("bg-dark-alfa-50 bg-dark-alfa-70")
 }
 
 // Light Mode
 function setLightMode() {
     $("#DarkLightToggle").text("Dark Mode");
     $(".page").removeClass("bg-dark light-content")
+    $(".home-section, .small-section, .main-footer-div").removeClass("bg-dark-alfa-70")
+    $(".home-section, .small-section, .main-footer-div").addClass("bg-dark-alfa-50")
     $(".main-nav").addClass("light-after-scroll")
     $(".divider").removeClass("white")
     $(".page-section").removeClass("bg-dark light-content")
-    $(".home-section").toggleClass("bg-dark-alfa-50 bg-dark-alfa-70")
     $("#work-grid").addClass("hover-white")
-    $(".small-section").toggleClass("bg-dark-alfa-50 bg-dark-alfa-70")
     $(".call-action-btn").removeClass("btn-w")
     $(".submit_btn").removeClass("btn-w")
-    $(".main-footer-div").toggleClass("bg-dark-alfa-50 bg-dark-alfa-70")
 }
 
 function DarkLightToggle() {
