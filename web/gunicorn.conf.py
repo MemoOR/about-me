5b353f2839612e8912dedcec55be5430a7ed104e
MemoOR <dethkarz2009@hotmail.com>
2023-11-18 00:02:34 -0600

    remove www to non www redirect

--- web/gunicorn.conf.py
+++ web/gunicorn.conf.py
@@ -144,7 +144,7 @@ tmp_upload_dir = None
 #
 
 errorlog = '-'
-loglevel = 'debug'
+loglevel = 'warning'
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
