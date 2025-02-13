8c04d18d18b1e549035a1e344a26273754699092
MemoOR <dethkarZ2009@hotmail.com>
2025-02-13 00:13:43 -0600

    update script to deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@
 cd /opt/app/about-me
 sudo git pull
 
-source /opt/app/gdcode-webpage/app.env
+source /opt/app/about-me/app.env
 echo "$DOCKER_LOGIN_PASSWORD" | docker login -u $DOCKER_LOGIN_USERNAME --password-stdin
 make deploy
-sudo rm /opt/app/gdcode-webpage/app.env
+sudo rm /opt/app/about-me/app.env
--- deploy/update_certificate.sh
+++ deploy/update_certificate.sh
@@ -3,8 +3,8 @@
 cd /opt/app/about-me
 sudo git pull
 
-source /opt/app/gdcode-webpage/app.env
+source /opt/app/about-me/app.env
 echo "$DOCKER_LOGIN_PASSWORD" | docker login -u $DOCKER_LOGIN_USERNAME --password-stdin
 make renew-cert
 make deploy
-sudo rm /opt/app/gdcode-webpage/app.env
+sudo rm /opt/app/about-me/app.env
